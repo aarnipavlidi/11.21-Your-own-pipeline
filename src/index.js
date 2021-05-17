4893013139bb565575c716930d34813d683c9612
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-05-17 18:59:17 +0300

    anekdootit_step07

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@
 
 import React from 'react' // Sovellus ottaa "react" nimisen kirjaston käyttöönsä.
 import ReactDOM from 'react-dom' // Sovellus ottaa "react-dom" nimisen kirjaston käyttöönsä.
-import { createStore } from 'redux' // Sovellus ottaa "createStore" funktion käyttöönsä => "redux" kirjaston kautta.
+
 import { Provider } from 'react-redux' // Sovellus ottaa "Provider" komponentin käyttöönsä => "react-redux" kirjaston kautta.
 import App from './App' // Sovellus ottaa "App.js" tiedoston sisällön käyttöönsä.
-import reducer from './reducers/anecdoteReducer' // Alustetaan muuttuja "reducer", joka hyödyntää "anecdoteReducer.js" tiedoston sisältöä sovelluksen aikana.
 
-// Alustetaan muuttuja "store", joka hyödyntää => "createStore()" funktiota.
-const store = createStore(reducer) // Funktio käyttää parametrinä "reducer" muuttujan arvoa, lisää funktiosta löytyy täältä: https://redux.js.org/api/createstore
+import store from './store' // Alustetaan muuttuja "store", joka ottaa "store.js" tiedoston sisällön käyttöönsä.
 
 // Sovellus renderöi käyttäjälle alla olevat komponentit näkyviin, joiden sisältö
 // siirtyvät => "index.html" tiedostoon => elementin sisälle, jonka id:n arvo on "root".
