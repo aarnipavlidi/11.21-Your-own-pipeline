078310d30924abc1356958224690ac863d545703
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-05-19 23:26:23 +0300

    anekdootit_connect_step01

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,14 +2,9 @@
 // then please contact me by sending email at me@aarnipavlidi.fi <3
 
 import React from 'react'; // Komponentti ottaa "react" nimisen kirjaston käyttöönsä.
-import { useSelector } from 'react-redux' // Komponentti ottaa "useSelector" funktion käyttöönsä => "react-redux" kirjaston kautta.
+import { connect } from 'react-redux' // Komponentti ottaa "connect" funktion käyttöönsä => "react-redux" kirjaston kautta.
 
-const Notification = () => { // Alustetaan "Notification" niminen komponentti, joka suorittaa {...} sisällä olevat asiat.
-  // Alustetaan muuttuja "getNotificationContent", joka suorittaa "useSelector(...)" funkion. Tämän avulla päästään
-  // käsiksi "storeen" tallennettuun taulukon arvoihin. Lisää tästä täältä: https://react-redux.js.org/api/hooks#useselector
-  // Ota myös huomioon, että käytämme alla olevan funktion sisällä => "state.message",
-  // koska olemme asettaneet aikaisemmin storeen (store.js) kyseisen objektin arvon.
-  const getNotificationContent = useSelector(state => state.message)
+const Notification = (props) => { // Alustetaan "Notification" niminen komponentti, joka suorittaa {...} sisällä olevat asiat.
 
   const style = { // Alustetaan muuttuja "style", joka saa käyttöönsä {...} sisällä olevien objektien arvot.
     border: 'solid',
@@ -17,23 +12,49 @@ const Notification = () => { // Alustetaan "Notification" niminen komponentti, j
     borderWidth: 1
   }
 
-  // Jos alla oleva if-ehto toteutuu eli "getNotificationContent" on yhtä kuin => "null", niin komponentti renderöi {..} sisällä olevan asian.
-  if (getNotificationContent === null) {
-    return null
+  const notificationToShow = () => { // Alustetaan muuttuja "notificationToShow(...)", joka suorittaa {...} sisällä olevat asiat.
+    // Jos alla oleva if-ehto toteutuu, eli "props.message" muuttuja on
+    // yhtä kuin => "null", niin renderöidään takaisin arvo => "null".
+    if (props.message === null) {
+      return null
+    }
+
+    // Jos alla oleva if-ehto toteutuu, eli "props.message" muuttuja on
+    // epätosi kuin => "null", niin renderöidään takaisin (...) sisällä
+    // olevat asiat. Elementti <h3> saa sen hetkisen "props.message" arvon.
+    if (props.message != null ) {
+      return (
+        <div style={style}>
+          <h3>{props.message}</h3>
+        </div>
+      )
+    }
   }
 
-  // Jos alla oleva if-ehto toteutuu eli "getNotificationContent" on muuta arvoa kuin => "null",
-  // niin komponentti renderöi {...} sisällä olevat asiat näkyviin käyttäjälle.
-  if (getNotificationContent != null) {
-    return (
-      <div style={style}>
-        <div>
-          <h3>{getNotificationContent}</h3>
-        </div>
-      </div>
-    )
+  // Komponentti renderöi (...) sisällä olevan funktion takaisin käyttäjälle.
+  return (
+    <div>
+      {notificationToShow()}
+    </div>
+  )
+}
+
+// Alustetaan muuttuja "mapStateToProps", joka suorittaa {...} sisällä olevat asiat.
+// Muuttuja palauttaa meille takaisin datan, mikä sijaitsee storessa "message"
+// objektin alla. Funktio käyttää myös parametrinä "state" muuttujan arvoa.
+// Lisää infoa funktiosta => https://react-redux.js.org/using-react-redux/connect-mapstate#defining-mapstatetoprops
+const mapStateToProps = (state) => {
+  return {
+    message: state.message
   }
 }
 
-// Viedään (export) alla oleva komponentti (Notification) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
-export default Notification
+// Alustetaan muuttuja "ConnectedNotification", jonka avulla kyseinen komponentti
+// eli "Notification" yhdistetään storen datan kanssa => "mapStateToProps()"
+// muuttujan avulla. Tämä tarkoittaa sitä, että muuttuja "props.message"
+// on yhtä kuin => "state.message". Jos tätä alla olevaa muuttujaa ei olisi,
+// niin sovellus ei pystyisi renderöimään storessa olevaa dataa takaisin näkyviin.
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+// Viedään (export) alla oleva komponentti (ConnectedNotification) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
+export default ConnectedNotification
