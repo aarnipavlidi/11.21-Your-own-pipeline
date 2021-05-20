5605d080aa2153ad51f8425a8a24c2642c81eb00
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-05-20 16:26:11 +0300

    anekdootit_connect_step02

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -2,16 +2,12 @@
 // then please contact me by sending email at me@aarnipavlidi.fi <3
 
 import React from 'react'; // Komponentti ottaa "react" nimisen kirjaston käyttöönsä.
-import { useDispatch } from 'react-redux' // Komponentti ottaa "useDispatch" funktion käyttöönsä => "react-redux" kirjaston kautta.
+import { connect } from 'react-redux' // Komponentti ottaa "connect" funktion käyttöönsä => "react-redux" kirjaston kautta.
 
 import { createNewValue } from '../reducers/anecdoteReducer' // Komponentti ottaa "createNewValue" funktion käyttöönsä, joka sijaitsee => "anecdoteReducer.js" tiedostossa.
 import { showNotificationMessage } from '../reducers/notificationReducer' // Komponentti ottaa "showNotificationMessage" funktion käyttöönsä, joka sijaitsee => "anecdoteReducer.js" tiedostossa.
 
-const AnecdoteForm = () => { // Alustetaan "AnecdoteForm" niminen komponentti, joka suorittaa {...} sisällä olevat asiat.
-  // Alustetaan muuttuja "dispatch", joka suorittaa "useDispatch()" funktion. Tämän avulla sovellus
-  // pystyy tekemään muutoksia "storeen" tallennettuihin  taulukon arvoihin. Lisää tästä täältä: https://react-redux.js.org/api/hooks#usedispatch
-  const dispatch = useDispatch()
-
+const AnecdoteForm = (props) => { // Alustetaan "AnecdoteForm" niminen komponentti, joka suorittaa {...} sisällä olevat asiat.
   // Alustetaan muuttuja "addValue", joka suorittaa {...} sisällä olevat asiat,
   // aina kun kyseiseen funktioon tehdään viittaus eli aina kun, käyttäjä klikkaa
   // painiketta, niin suoritetaan kyseinen funktio ja lopuksi viedään tiedot
@@ -23,8 +19,8 @@ const AnecdoteForm = () => { // Alustetaan "AnecdoteForm" niminen komponentti, j
     // Ota myös huomioon, että alla olevan input:in => "name" arvo on yhtä kuin "newText", jos sitä ei olisi tai on väärä, niin tulee erroria! :)
     const getContent = event.target.newText.value // Alustetaan muuttuja "getContent", joka saa sen hetkisen arvon, minkä käyttäjä laittaa => input:in arvoksi.
     event.target.newText.value = '' // Sovellus tyhjentää kyseisen input:in arvon, sen jälkeen kun yllä oleva muuttuja on saanut arvon.
-    dispatch(createNewValue(getContent)) // Sovellus suorittaa funktion "createNewValue(...)", joka saa parametrin "getContent" muuttujan arvon.
-    dispatch(showNotificationMessage(`You have added ${getContent} to the database. Thank you for adding!`, 10)) // Sovellus suorittaa funktion "showNotificationMessage(...)", joka saa parametrin "getContent" muuttujan arvon.
+    props.createNewValue(getContent) // Suoritetaan kyseinen funktio (dispatch(...)) props muuttujan sekä "mapDispatchToProps(...)" muuttujan avulla.
+    props.showNotificationMessage(`You have added ${getContent} to the database. Thank you for adding!`, 10) // Suoritetaan kyseinen funktio (dispatch(...)) props muuttujan sekä "mapDispatchToProps(...)" muuttujan avulla.
   }
 
   // Komponentti renderöi käyttäjälle näkyviin (...) sisällä olevat asiat.
@@ -38,5 +34,38 @@ const AnecdoteForm = () => { // Alustetaan "AnecdoteForm" niminen komponentti, j
   )
 }
 
-// Viedään (export) alla oleva komponentti (AnecdoteForm) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
-export default AnecdoteForm
+// Alustetaan muuttuja "mapStateToProps", joka suorittaa {...} sisällä olevat asiat.
+// Muuttuja palauttaa meille takaisin datan, mikä sijaitsee storessa "filter"
+// objektin alla. Funktio käyttää myös parametrinä "state" muuttujan arvoa.
+// Lisää infoa funktiosta => https://react-redux.js.org/using-react-redux/connect-mapstate#defining-mapstatetoprops
+const mapStateToProps = (state) => {
+  return {
+    values: state.values,
+    message: state.message
+  }
+}
+
+// Alustetaan muuttuja "mapDispatchToProps", joka suorittaa {...} sisällä olevat asiat.
+// Muuttujan avulla komponentti voi suorittaa "createNewValue(...)" actionin eli
+// dispatchaa ja siirtyy suorittamaan sen sisältöä => "anecdoteReducer.js" tiedostossa.
+// Tämän lisäksi komponentti voi dispatchaa => "showNotificationMessage(...)" muuttujan
+// ja suorittaa sen sisällön => "Notification.js" tiedostossa. Nyt molemmat muuttujat
+// voidaan suorittaa => "props.funktio(parametri)" kautta, kun aikaisemmissa
+// tehtävissä molemmat muuttujat suoritettiin => "dispatch(funktio(parametri))".
+const mapDispatchToProps = {
+  createNewValue,
+  showNotificationMessage,
+}
+
+// Alustetaan muuttuja "ConnectedAnecdoteForm", jonka avulla kyseinen komponentti
+// eli "AnecdoteForm" yhdistetään storen datan kanssa => "mapStateToProps()"
+// muuttujan avulla. Tämä tarkoittaa sitä, että muuttuja "props.values/message"
+// on yhtä kuin => "state.values/message". Jos tätä alla olevaa muuttujaa ei olisi,
+// niin sovellus ei pystyisi renderöimään storessa olevaa dataa takaisin näkyviin.
+const ConnectedAnecdoteForm = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AnecdoteForm)
+
+// Viedään (export) alla oleva komponentti (ConnectedAnecdoteForm) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
+export default ConnectedAnecdoteForm
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,34 +2,54 @@
 // then please contact me by sending email at me@aarnipavlidi.fi <3
 
 import React from 'react'; // Komponentti ottaa "react" nimisen kirjaston käyttöönsä.
-import { useDispatch } from 'react-redux' // Komponentti ottaa "useDispatch" funktion käyttöönsä => "react-redux" kirjaston kautta.
+import { connect } from 'react-redux' // Komponentti ottaa "connect" funktion käyttöönsä => "react-redux" kirjaston kautta.
 
 import { changeFilterValue } from '../reducers/filterReducer' // Komponentti ottaa "changeFilterValue" funktion käyttöönsä, joka sijaitsee => "filterReducer.js" tiedostossa.
 
-const Filter = () => { // Alustetaan "Filter" niminen komponentti, joka suorittaa {...} sisällä olevat asiat.
-  // Alustetaan muuttuja "dispatch", joka suorittaa "useDispatch()" funktion. Tämän avulla sovellus
-  // pystyy tekemään muutoksia "storeen" tallennettuihin  taulukon arvoihin. Lisää tästä täältä: https://react-redux.js.org/api/hooks#usedispatch
-  const dispatch = useDispatch()
-
-  const handleFilterInput = (event) => { // Alustetaan muuttuja "handleFilterInput", joka suorittaa {...} sisällä olevat asiat aina kun kyseiseen funktioon tehdään viittaus.
-    // Muuttuja "event.target.value", tarkoittaa siis sitä, että kaikki
-    // minkä käyttäjä kirjoittaa input:iin, niin sen hetkinen arvo
-    // tallentuu kyseisen muuttujan alle eli "getCurrentFilterValue".
-    const getCurrentFilterValue = event.target.value // Alustetaan muuttuja "getCurrentFilterValue", joka on yhtä kuin "event.target.value"
-    dispatch(changeFilterValue(getCurrentFilterValue)) // Sovellus suorittaa funktion "changeFilterValue(...)", joka saa parametrin "getCurrentFilterValue" muuttujan arvon.
-  }
+const Filter = (props) => { // Alustetaan "Filter" niminen komponentti, joka suorittaa {...} sisällä olevat asiat.
 
   const style = { // Alustetaan muuttuja "style", joka saa käyttöönsä {...} sisällä olevien objektien arvot.
     marginBottom: 10
   }
 
-  // Komponentti renderöi käyttäjälle näkyviin (...) sisällä olevat asiat.
+  // Komponentti renderöi käyttäjälle näkyviin (...) sisällä olevat asiat. Aina kun
+  // käyttäjä kirjoittaa jotain input:in arvoon, niin sovellus suorittaa funktion =>
+  // "changeFilterValue(...)" props sekä "mapStateToProps(...)" muuttujan avulla.
   return (
     <div style={style}>
-      <div>Filter: <input onChange={handleFilterInput} /></div>
+      <div>Filter: <input onChange={ (event) => props.changeFilterValue(event.target.value)} /></div>
     </div>
   )
 }
 
-// Viedään (export) alla oleva komponentti (Filter) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
-export default Filter
+// Alustetaan muuttuja "mapStateToProps", joka suorittaa {...} sisällä olevat asiat.
+// Muuttuja palauttaa meille takaisin datan, mikä sijaitsee storessa "filter"
+// objektin alla. Funktio käyttää myös parametrinä "state" muuttujan arvoa.
+// Lisää infoa funktiosta => https://react-redux.js.org/using-react-redux/connect-mapstate#defining-mapstatetoprops
+const mapStateToProps = (state) => {
+  return {
+    filter: state.filter
+  }
+}
+
+// Alustetaan muuttuja "mapDispatchToProps", joka suorittaa {...} sisällä olevat asiat.
+// Muuttujan avulla komponentti voi suorittaa "changeFilterValue(...)" actionin eli
+// dispatchaa ja siirtyy suorittamaan sen sisältöä => "filterReducer.js" tiedostossa.
+// Nyt meillä on pelkästään => "props.changeFilterValue(...)" ja aikaisemmissa
+// tehtävissä kyseinen funktio oli muotoa => "dispatch(changeFilterValue(...))".
+const mapDispatchToProps = {
+  changeFilterValue,
+}
+
+// Alustetaan muuttuja "ConnectedFilter", jonka avulla kyseinen komponentti
+// eli "Filter" yhdistetään storen datan kanssa => "mapStateToProps()"
+// muuttujan avulla. Tämä tarkoittaa sitä, että muuttuja "props.filter"
+// on yhtä kuin => "state.filter". Jos tätä alla olevaa muuttujaa ei olisi,
+// niin sovellus ei pystyisi renderöimään storessa olevaa dataa takaisin näkyviin.
+const ConnectedFilter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Filter)
+
+// Viedään (export) alla oleva komponentti (ConnectedFilter) sovelluksen käytettäväksi, jotta esim. "App.js" tiedosto pystyy suorittamaan kyseiset funktiot.
+export default ConnectedFilter
